d32ee29e8b45f6fe54d97db86038d91e32e1d06d
Richard da Silva Rodrigues <182400719@senacinformatica.br>
2025-01-20 14:34:37 -0300

    versão 1.1

--- nome_do_projeto/appgoku/urls.py
+++ nome_do_projeto/appgoku/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('start-strength/', views.start_strength_training, name='start_strength'),
+    path('start-cardio/', views.start_cardio, name='start_cardio'),
+    path('track-progress/', views.track_progress, name='track_progress'),
 ]
--- nome_do_projeto/appgoku/views.py
+++ nome_do_projeto/appgoku/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import JsonResponse
+
 # Create your views here.
 def index(request):
     return render(request, 'appgoku/home.html')
+
+def start_strength_training(request):
+    # Simulated strength training start
+    messages.success(request, "🏋️ Treino de Força iniciado! Vamos aumentar seu poder de luta!")
+    return redirect('home')
+
+def start_cardio(request):
+    # Simulated cardio training start
+    messages.success(request, "🏃 Corrida iniciada! Transformando-se em Super Saiyajin!")
+    return redirect('home')
+
+def track_progress(request):
+    # Simulated progress tracking
+    progress_data = {
+        'strength_level': 75,
+        'cardio_endurance': 65,
+        'power_level': 'Super Saiyajin Nível 1'
+    }
+    return JsonResponse(progress_data)
